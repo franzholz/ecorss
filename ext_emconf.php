bf26dadf5ba1a568a1ccea07c8fe83021864a769
Franz Holzinger <franz.holzinger@festwein.de>
2024-05-29 19:49:15 +0200

    PHP 8 is minimum

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@
 # Extension Manager/Repository config file for ext "ecorss".
 ########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Ecodev : feeds services (RSS / ATOM)',
     'description' => 'Generate easily RSS / ATOM feeds based on the latest content of any tables in the database. Can deal with flexform content and multilingual / multidomain websites.',
     'category' => 'fe',
@@ -15,17 +15,17 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Franz Holzinger, Fabien Udriot',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
-    'constraints' => array(
-        'depends' => array(
-            'php' => '7.4.0-8.4.99',
+    'constraints' => [
+        'depends' => [
+            'php' => '8.0.0-8.4.99',
             'typo3' => '11.5.0-12.4.99',
             'div2007' => '1.17.0-0.0.0',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
             'typo3db_legacy' => '1.0.0-1.1.99',
-        ),
-    ),
-);
+        ],
+    ],
+];
 
